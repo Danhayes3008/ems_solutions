c76f871247422750df002ff9dabe812aae67746d
danhayes <danhayes3008@outlook.com>
2021-05-08 12:39:56 +0100

    created the route that will get the information from my employee table
    added some comments

--- server/index.js
+++ server/index.js
@@ -4,7 +4,23 @@ const cors = require("cors");
 
 //  middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // allows us to use req.body
+
+// ROUTES
+
+// get all employee's from the employee table
+app.get("/employee/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const todo = await pool.query("SELECT * FROM employee WHERE id = $1", [
+        id
+      ]);
+
+      res.json(todo.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
 
 // this prints a message to the console when the server starts
 app.listen(5000, () => {
