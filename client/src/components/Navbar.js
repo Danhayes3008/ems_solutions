deeba07b1f6c1abaedc15a81818539d871b123d2
danhayes <danhayes3008@outlook.com>
2021-05-08 13:58:04 +0100

    initial commit of my navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <div>
+                    <h1>wk - 32</h1>
+                    <h3>Daniel Hayes</h3>
+                    <h3>30-04-2021 13:42:27</h3>
+                </div>
+                <div>
+                    <ul>
+                        <li>personnel</li>
+                        <li>payroll</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
