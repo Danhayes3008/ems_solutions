63a7c58230e9ef6a91a13c6d2fb5db532c9e9278
danhayes <danhayes3008@outlook.com>
2021-05-08 14:00:18 +0100

    started working on the react routers so that i can navigate to the different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React, { Fragment } from "react";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+// Components
+import Navbar from "./components/Navbar"
+import Personnel from "./components/Personnel"
+import Payroll from "./components/Payroll"
 
 function App() {
   return (
-    <Fragment></Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route path="/personnel" component={Personnel} />
+        <Route path="/payroll" component={Payroll} />
+      </Fragment>
+    </Router>
   );
 }
 
