0cd5a7951b1debfc0f069b36295d10568fa5eab1
danhayes <danhayes3008@outlook.com>
2021-05-10 15:36:54 +0100

    some adjustments to some imports and set my routers to keep the navbar out of the login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
 import React, { Fragment } from "react";
-import './App.css';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import "./App.css"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import "./styles/scss/main.css";
 
 // Components
+import Login from "./components/Login"
 import Navbar from "./components/Navbar"
 import Personnel from "./components/Personnel"
 import Payroll from "./components/Payroll"
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route path="/personnel" component={Personnel} />
-        <Route path="/payroll" component={Payroll} />
-      </Fragment>
-    </Router>
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <div>
+            <Navbar />
+            <Route path="/personnel" component={Personnel} />
+            <Route path="/payroll" component={Payroll} />
+          </div>
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
