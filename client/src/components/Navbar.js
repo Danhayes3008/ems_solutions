799410586a548db414fba1822afc7afb7e988324
danhayes <danhayes3008@outlook.com>
2021-05-10 13:37:10 +0100

    added some class names and put the list content in a p tag

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,21 +4,23 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <nav>
-                <div>
+                <div className="navbar-details">
                     <h1>wk - 32</h1>
                     <h3>Daniel Hayes</h3>
                     <h3>30-04-2021 13:42:27</h3>
                 </div>
                 <div>
                     <ul>
-                        <li>Home</li>
-                        <li>Schedules</li>
-                        <li>Requests</li>
-                        <li>Exceptions</li>
-                        <li>Payroll</li>
-                        <li>Forcasting</li>
-                        <li>Personnel</li>
-                        <li>Logout</li>
+                        <li>
+                            <p className="list-item">Home</p>
+                        </li>
+                        <li><p className="list-item">Schedules</p></li>
+                        <li><p className="list-item">Requests</p></li>
+                        <li><p className="list-item">Exceptions</p></li>
+                        <li><p className="list-item">Payroll</p></li>
+                        <li><p className="list-item">Forcasting</p></li>
+                        <li><p className="list-item">Personnel</p></li>
+                        <li><p className="list-item">Logout</p></li>
                     </ul>
                 </div>
             </nav>
