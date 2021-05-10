4c63950aeab6c78b45242712a063e053ef23183f
danhayes <danhayes3008@outlook.com>
2021-05-10 17:02:11 +0100

    initial commit

---
+++ client/src/components/Exceptions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Exceptions = () => {
+    return (
+        <div className="container">
+            <h1>Exceptions</h1>
+        </div>
+    )
+}
+
+export default Exceptions
---
+++ client/src/components/Forcasting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Forcasting = () => {
+    return (
+        <div className="container">
+            <h1>Forcasting</h1>
+        </div>
+    )
+}
+
+export default Forcasting 
