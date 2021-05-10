11583c1befd106005af49446bab8283fe23e05a9
danhayes <danhayes3008@outlook.com>
2021-05-10 16:53:50 +0100

    finished importing all the routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,13 @@ import "./styles/scss/main.css";
 // Components
 import Login from "./components/Login"
 import Navbar from "./components/Navbar"
+import Home from "./components/Home"
 import Schedules from "./components/Schedules"
-import Personnel from "./components/Personnel"
+import Requests from "./components/Requests"
+import Exceptions from "./components/Exceptions"
 import Payroll from "./components/Payroll"
+import Forcasting from "./components/Forcasting"
+import Personnel from "./components/Personnel"
 
 function App() {
   return (
@@ -18,9 +22,12 @@ function App() {
           <Route path="/login" component={Login} />
           <div>
             <Navbar />
-            <Route path="/" />
+            <Route path="/home" component={Home} />
             <Route path="/schedules" component={Schedules} />
+            <Route path="/requests" component={Requests} />
+            <Route path="/exceptions" component={Exceptions} />
             <Route path="/payroll" component={Payroll} />
+            <Route path="/forcasting" component={Forcasting} />
             <Route path="/personnel" component={Personnel} />
           </div>
         </Switch>
