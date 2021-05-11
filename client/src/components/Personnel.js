c2dde43ba39ac02fd5e885f1f44825d24e329dc9
danhayes <danhayes3008@outlook.com>
2021-05-11 13:02:08 +0100

    Created the template for my table section

--- client/src/components/Personnel.js
+++ client/src/components/Personnel.js
@@ -3,7 +3,24 @@ import React from 'react'
 const Personnel = () => {
     return (
         <div className="container">
+            <div className="box">
             <h1>Personnel</h1>
+            <table>
+                <tr>
+                    <th>Staff Number</th>
+                    <th>Employee</th>
+                    <th>Dipartment</th>
+                    <th>Role</th>
+                </tr>
+                    <tr>
+                        {/* fake info to get the table working */}
+                    <td>102422</td>
+                    <td>Hayes, Daniel</td>
+                    <td>Management</td>
+                    <td>Store Manager</td>
+                </tr>
+            </table>
+            </div>
         </div>
     )
 }
