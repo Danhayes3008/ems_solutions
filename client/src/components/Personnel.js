e856d58f371087949bace518ddb74f6bef14c9c8
danhayes <danhayes3008@outlook.com>
2021-05-13 14:33:38 +0100

    got my employee table to display
    i wrapped the content on the page with a fragment tag to be consistent. adjusted the employees.map to get the information to display correctly and removed hard coded table rows of employees as i do not need it now

--- client/src/components/Personnel.js
+++ client/src/components/Personnel.js
@@ -1,61 +1,56 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 const Personnel = () => {
+    const [employees, setEmployee] = useState([]);
+  const getEmployees = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/employees");
+      const jsonData = await response.json();
+
+      setEmployee(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getEmployees();
+    // this [] stops the use effect from constently logging the array
+  }, []);
+
   return (
-    <div className="container">
-      <div className="box">
-        <h1>Personnel</h1>
-        <table>
-          <tr className="table-header">
-            <th>Staff Number</th>
-            <th>Employee</th>
-            <th>Dipartment</th>
-            <th>Role</th>
-            <th></th>
-          </tr>
-          <tr className="table-rows">
-            {/* fake info to get the table working */}
-            <td>102422</td>
-            <td>Hayes, Daniel</td>
-            <td>Management</td>
-            <td>Store Manager</td>
-            <td>
-              <button>Edit</button>
-            </td>
-                  </tr>
-                  <tr className="table-rows">
-            {/* fake info to get the table working */}
-            <td>102422</td>
-            <td>Hayes, Daniel</td>
-            <td>Management</td>
-            <td>Store Manager</td>
-            <td>
-              <button>Edit</button>
-            </td>
-                  </tr>
-                  <tr className="table-rows">
-            {/* fake info to get the table working */}
-            <td>102422</td>
-            <td>Hayes, Daniel</td>
-            <td>Management</td>
-            <td>Store Manager</td>
-            <td>
-              <button>Edit</button>
-            </td>
-                  </tr>
-                  <tr className="table-rows">
-            {/* fake info to get the table working */}
-            <td>102422</td>
-            <td>Hayes, Daniel</td>
-            <td>Management</td>
-            <td>Store Manager</td>
-            <td>
-              <button>Edit</button>
-            </td>
-          </tr>
-        </table>
+      <Fragment>
+          {""}
+      <div className="container">
+        <div className="box">
+          <h1>Personnel</h1>
+          <table>
+            <thead>
+            <tr className="table-header">
+              <th>Staff Number</th>
+              <th>Employee</th>
+              <th>Dipartment</th>
+              <th>Role</th>
+              <th></th>
+              </tr>
+            </thead>
+            <tbody>
+            {employees.map((staff) => (
+              <tr key={staff.employee_id} className="table-rows">
+                <td>{staff.staffnumber}</td>
+                <td>{staff.lastname}, {staff.firstname}</td>
+                <td>{staff.dipartment}</td>
+                <td>{staff.role}</td>
+                <td>
+                  <button>Edit</button>
+                </td>
+                </tr>
+            ))}
+              </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
