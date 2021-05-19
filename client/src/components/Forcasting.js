ec1011ae4ca7de9d871fd1209f911f7b231ac528
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2021-05-19 14:15:54 +0100

    created the table for my forecasting details. and connected it to my database

--- client/src/components/Forcasting.js
+++ client/src/components/Forcasting.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 
 const Forcasting = () => {
+    const [forcasting, setForcasting] = useState([]);
+  const getForcasting = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/forcasting");
+      const jsonData = await response.json();
+
+      setForcasting(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getForcasting();
+    // this [] stops the use effect from constently logging the array
+  }, []);
     return (
-        <div className="container">
-            <h1>Forcasting</h1>
+        <Fragment>
+          {""}
+      <div className="container">
+        <div className="box">
+          <h1>Forcasting</h1>
+          <table>
+            <thead>
+            <tr className="table-header">
+              <th>week Started</th>
+              <th>week end</th>
+              <th>wage budget</th>
+              <th>sales</th>
+              <th></th>
+              </tr>
+            </thead>
+            <tbody>
+            {forcasting.map((budget) => (
+              <tr key={budget.forcasting_id} className="table-rows">
+                <td>{budget.week_start}</td>
+                <td>{budget.week_end}</td>
+                <td>{budget.wage_budget}</td>
+                <td>{budget.sales}</td>
+                <td>
+                  <button>Edit</button>
+                </td>
+                </tr>
+            ))}
+              </tbody>
+          </table>
         </div>
+      </div>
+    </Fragment>
     )
 }
 
